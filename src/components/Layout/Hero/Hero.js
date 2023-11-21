7b2ee4467c77668ce5fcc6de0fe0c1a426ceb026
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2023-11-21 22:29:54 +0100

    finished the Nav

---
+++ src/components/Layout/Hero/Hero.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hero = () => {
+  return <section className="background h-screen">Hero</section>;
+};
+
+export default Hero;
---
+++ src/components/Layout/NavBar/Nav.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "../../../App.css";
+import Logo from "../../../Assets/Logo.png";
+import { TiArrowSortedDown } from "react-icons/ti";
+
+const Nav = () => {
+  return (
+    <section className="bg">
+      <nav className="h-[10.5rem]">
+        <div className="flex py-5 px-[3.5rem]">
+          <p className="text-[#017A59] pb-2 uppercase text-[1rem] px-5 border-b-[3px] border-[#017A59] font-semibold leading-normal tracking-[-0.02rem]">
+            For Individuals
+          </p>
+          <p className="text-[#017A59] pb-2 uppercase text-[1rem] px-5 border-b-[3px] border-[#017A59] font-semibold leading-normal tracking-[-0.02rem]">
+            For Business
+          </p>
+        </div>
+        <div className="flex justify-between px-[3.5rem] py-[1.17rem]">
+          <img src={Logo} alt="" className="w-[14.5rem] h-[2.5rem]" />
+          <ul className="flex space-x-[3.12rem] mt-3">
+            <li className="flex gap-1 text-[#1D1D1F] text-[1.0625rem] font-semibold leading-[1.5rem] tracking-[-0.02125rem]">
+              Products <TiArrowSortedDown className="mt-1" />{" "}
+            </li>
+            <li className="flex gap-1 text-[#1D1D1F] text-[1.0625rem] font-semibold leading-[1.5rem] tracking-[-0.02125rem]">
+              Company <TiArrowSortedDown className="mt-1" />
+            </li>
+            <li className="flex gap-1 text-[#1D1D1F] text-[1.0625rem] font-semibold leading-[1.5rem] tracking-[-0.02125rem]">
+              Support <TiArrowSortedDown className="mt-1" />
+            </li>
+          </ul>
+
+          <div className="flex space-x-[2rem]">
+            <p className="mt-3 text-[#1D1D1F] text-[1.0625rem] font-semibold leading-[1.5rem] tracking-[-0.02125rem]">
+              Sign in
+            </p>
+            <button className="bg-[#017A59] text-white px-5 py-3 rounded-xl  text-[1.125rem] font-semibold leading-[1.5rem] tracking-[-0.0225rem]">
+              Open an account
+            </button>
+          </div>
+        </div>
+      </nav>
+    </section>
+  );
+};
+
+export default Nav;
